1b214330a12b024d67092854a38bdf0b9b442821
hue <hue@gravitywell-2.local>
2011-09-04 20:48:33 -0400

    ignore textmate project; generalize run.sh for testing ruby 1.8 and 1.9 (both work); generalize for arbitrary paths (linux/mac); remove textmate project file

--- examples/run.sh
+++ examples/run.sh
@@ -1,1 +1,14 @@
-RUBYLIB=~/marley/lib ruby1.9 ./simple_forum.rb run
+#!/bin/sh
+
+# directory of the marley base directory
+MARLEY_DIR=..
+
+# directory of the marley libs
+MARLEY_LIB=$MARLEY_DIR/lib
+
+# ruby interpreter to use (1.8, 1.9)
+RUBY=ruby
+#RUBY=/opt/local/bin/ruby1.9
+
+RUBYLIB=$MARLEY_LIB $RUBY ./simple_forum.rb run
+
