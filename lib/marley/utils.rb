1682d69486c1107d4602d06f4dadd6c28f67f5ba
Herb Daily <herb.daily@safe-mail.net>
2011-11-22 20:33:04 -0300

    oops, forgot the new utils.rb file

---
+++ lib/marley/utils.rb
@@ -0,0 +1,10 @@
+
+module Marley
+  module Utils
+    def self.hash_keys_to_syms(hsh)
+      hsh.inject({}) {|h,(k,v)| h[k.to_sym]= v.class==Hash ? hash_keys_to_syms(v) : v;h }
+    end
+    def self.make_rest_hash
+    end
+  end
+end
