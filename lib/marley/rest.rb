20077726dcd36d8850b9786667732994fcd97ef9
Herb Daily <herb.daily@safe-mail.net>
2011-11-23 15:21:42 -0300

    rest module now working

--- lib/marley/rest.rb
+++ lib/marley/rest.rb
@@ -2,20 +2,23 @@
 module Marley
   module Rest
     def rest_opts(name)
-      meth_name="rest_#{name}"
+      meth_name="@_rest_#{name}"
       proc_name="#{meth_name}_proc"
-      define_method "rest_#{name}" do |*args|
-        if args[0]
-          instance_variable_set meth_name,args[0]
-          instance_variable_set proc_name,&block if block_given?
-        else
-          if instance_variable_get proc_name
-            instance_variable_get(meth_name)[instance_variable_get(proc_name).call]
+      m=Module.new do
+        define_method "rest_#{name}" do |*args|
+          if args[0]
+            instance_variable_set meth_name,args[0]
+            instance_variable_set proc_name,args[1] if args[1]
           else
-            instance_variable_get(meth_name)
+            if instance_variable_get proc_name
+              instance_variable_get(meth_name)[instance_variable_get(proc_name).call]
+            else
+              instance_variable_get(meth_name)
+            end
           end
         end
       end
+      extend m
     end
   end
 end
