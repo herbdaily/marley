9732935ad02d92d671a4c8cca8e967a1227b6ebd
Herb Daily <herb.daily@safe-mail.net>
2011-12-01 03:22:00 -0300

    start joints redesign

--- lib/marley/joint.rb
+++ lib/marley/joint.rb
@@ -5,11 +5,20 @@ module Marley
     #    - ClassMethods - Modules within this module will extend any constant in Marley::Resources with the same name.
     #    - InstanceMethods - Modules within this module will append their features to any constant in Marley::Resources with the same name. 
     class Joint
-      #def self.provides(*args)
-      #  [:resources,:class_methods,:]
-      #end
+      MODS=['Resources','ClassMethods','InstanceMethods']
+      IMPORTERS={
+       'Resources' => lambda {|c| MR.const_set(c,Resources.const_get(c))},
+       'ClassMethods' => lambda {|c| MR.const_get(c).extend klass::ClassMethods.const_get(c)},
+       'InstanceMethods' =>  lambda {|c| klass::InstanceMethods.const_get(c).send :append_features, MR.const_get(c)}
+      }
+      MODS.each {|mod| const_set(mod,Module.new)}
+      def self.mods
+        MODS.map {|mod_name| self.const_get(mod_name.to_s.camelize) } 
+      end
       def initialize(opts={})
         config(opts)
+      end
+      def self.resources
       end
       def smoke
         klass=self.class
@@ -17,10 +26,8 @@ module Marley
           'class_methods' => lambda {|c| MR.const_get(c).extend klass::ClassMethods.const_get(c)},
           'instance_methods' => lambda {|c| klass::InstanceMethods.const_get(c).send :append_features, MR.const_get(c)}
         }.each_pair do |mod_name, importer|
-          if klass.constants.include?(mod_name.camelize)
-            klass.const_get(mod_name.camelize).constants.each do |c|
-              importer.call(c) unless @opts[mod_name.to_sym] && ! @opts[mod_name.to_sym].include?(c.underscore)
-            end
+          klass.const_get(mod_name.camelize).constants.each do |c|
+            importer.call(c) unless @opts[mod_name.to_sym] && ! @opts[mod_name.to_sym].include?(c.underscore)
           end
         end
         self
