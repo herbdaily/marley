22d1fb732b6e21ee526ff38b4a752ca0b6dc5c5e
Herb Daily <herb.daily@safe-mail.net>
2011-12-02 22:18:46 -0300

    ditch moderator class; admin is next

--- lib/joints/tagged_messaging.rb
+++ lib/joints/tagged_messaging.rb
@@ -21,9 +21,6 @@ module Marley
         class Admin < User 
           def self.requires_user?;true;end
         end
-        class Moderator < User
-          def self.requires_user?;true;end
-        end
         class PrivateMessage < MJ::Messaging::Resources::PrivateMessage
           attr_accessor :tags
           @actions_get= superclass.actions_get << 'new_tags'
