a1ce587f4aee87415e066b26f1ea78a4957ec928
Herb Daily <herb.daily@safe-mail.net>
2011-12-02 21:07:25 -0300

    no major changes, just about to do something dangerous

--- lib/marley/joint.rb
+++ lib/marley/joint.rb
@@ -1,9 +1,5 @@
 module Marley
   module Joints
-    # @abstract Subclass and implement one or more of the following modules within the class namespace:
-    #    - Resources - All constansts in this module will be imported into Marley::Resources
-    #    - ClassMethods - Modules within this module will extend any constant in Marley::Resources with the same name.
-    #    - InstanceMethods - Modules within this module will append their features to any constant in Marley::Resources with the same name. 
     class Joint
       def initialize(opts={})
         config(opts)
