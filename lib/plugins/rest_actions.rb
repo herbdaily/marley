032c916bdfe25a41791d517ce77eccd06660f4cc
Herb Daily <herb.daily@safe-mail.net>
2011-12-04 01:20:26 -0300

    some bug fixes

--- lib/plugins/rest_actions.rb
+++
@@ -1,8 +0,0 @@
-
-module Marley
-  module Plugins
-    module RestActions
-     InstanceActions=Marley::Utils.rest_opts_mod('actions',['get','post','put','delete'],lambda {$request[:user].class})
-    end
-  end
-end
