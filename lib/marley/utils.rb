a3e8ef4eae77e21d630dfd10a9b225286df48e95
Herb Daily <herb.daily@safe-mail.net>
2012-01-10 03:58:17 -0300

    ditch lots of old cruft

--- lib/marley/utils.rb
+++ lib/marley/utils.rb
@@ -38,33 +38,5 @@ module Marley
     def self.hash_keys_to_syms(hsh)
       hsh.inject({}) {|h,(k,v)| h[k.to_sym]= v.class==Hash ? hash_keys_to_syms(v) : v;h }
     end
-    # Deprecated.
-    def self.rest_opts_mod(name,opts,key_proc)
-      Module.new do |m|
-        @create_opts=[name,opts,key_proc]
-        def self.create_opts
-          @create_opts
-        end
-        def self.new(name=nil,opts=nil,key_proc=nil)
-          Marley::Utils.rest_opts_mod(*@create_opts)
-        end
-        opts.each {|opt| attr_accessor "#{name}_#{opt}"}
-        define_method "rest_#{name}" do
-          if opts.find {|opt| send(:"#{name}_#{opt}").to_s > ""}
-            foo=opts.inject({}) do |h,k|
-              i=send("#{name}_#{k}".sub(/^_/,''))
-              h[k.to_sym]=i.class==Hash ? i[key_proc.call] : i
-              h
-            end
-            if Marley.constants.include?("Reggae#{name.camelcase}")
-              Marley.const_get(:"Reggae#{name.camelcase}").new(foo)
-            else
-              foo 
-            end
-          end
-        end
-      end
-    end
   end
-  RestActions=Utils.rest_opts_mod('actions',['get','post','put','delete'],lambda {$request[:user].class})
 end
--- test/menu_tests.rb
+++
@@ -1,9 +0,0 @@
-
-
-class MenuTests < Test::Unit::TestCase
-  def setup
-  end
-  should 'work' do
-    assert true
-  end
-end
