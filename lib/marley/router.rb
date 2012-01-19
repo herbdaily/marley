6fce0d362479eba00e74bf62e3f09ce5c81837ff
Herb Daily <herb.daily@safe-mail.net>
2012-01-19 19:21:49 -0300

    change authentication errors to authorization errors where appropriate

--- lib/marley/router.rb
+++ lib/marley/router.rb
@@ -13,20 +13,26 @@ module Marley
       $request[:content_type]=request.xhr? ? 'application/json' : env['HTTP_ACCEPT'].to_s.sub(/,.*/,'') 
       $request[:content_type]='text/html' unless $request[:content_type] > ''
       $request[:content_type]='application/json' if env['rack.test']==true #there has to be a better way to do this...
-      @opts[:authenticate].call(env) if @opts[:authenticate]
-      $request[:path]=request.path.sub(/\/\/+/,'/').split('/')[1..-1]
+
       verb=request.request_method.downcase
       verb=$request[:post_params].delete(:_method).match(/^(put|delete)$/i)[1] rescue verb 
-      $request[:verb]="rest_#{verb}"
+      $request[:path]=request.path.sub(/\/\/+/,'/').split('/')[1..-1]
       rn=$request[:path] ? $request[:path][0].camelize : @opts[:default_resource]
-      raise AuthenticationError if (@opts[:authenticate] && $request[:user].new? && ! Resources.constants.include?(rn))
-      raise RoutingError unless Resources.const_defined?(rn)
+      $request[:verb]="rest_#{verb}"
+
+      @opts[:authenticate].call(env) if @opts[:authenticate]
+      unless Resources.const_defined?(rn)
+        raise AuthorizationError if (@opts[:authenticate] && $request[:user].new?)
+        raise RoutingError 
+      end
       resource=Resources.const_get(rn)
-      raise AuthenticationError if @opts[:authenticate] && resource.respond_to?('requires_user?') && resource.requires_user? && $request[:user].new?
+      raise AuthorizationError if @opts[:authenticate] && resource.respond_to?('requires_user?') && resource.requires_user? && $request[:user].new?
+
       controller=nil #clear from previous call
       controller=resource.controller if resource.respond_to?(:controller)
       controller=resource if resource.respond_to?($request[:verb]) 
       raise RoutingError unless controller
+
       json=controller.send($request[:verb]).to_json
       html=@opts[:client] ? @opts[:client].to_s(json) : json
       resp_code=RESP_CODES[verb]
