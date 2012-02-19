ce59af1276292cc86680fa7eeaf31ecb5fcc764b
Herb Daily <herb.daily@safe-mail.net>
2012-02-18 23:20:24 -0300

    more work on forum joint

--- lib/marley/joints/forum.rb
+++ lib/marley/joints/forum.rb
@@ -11,10 +11,14 @@ module Marley
           self.dataset.filter(:parent_id => nil)
         end
         def list(params=nil)
+          p topics.map{|t| t.thread}
           topics.map{|t| t.thread}
         end
       end
       module InstanceMethods
+        def write_cols
+          super.push(:topic_id, :parent_id)
+        end
         def topic_msg
           self.class.topics.filter(:topic_id => topic_id).first
         end
@@ -25,11 +29,19 @@ module Marley
           self.class.filter(:parent_id => id).all
         end
         def thread
-          children.length > 0 ? reggae_instance << children.map{|m| m.thread} : reggae_instance
+          return reggae_instance if children.length==0
+          foo=reggae_instance
+          foo[2] = children.map{|m| m.thread} 
+          foo
+        end
+        def before_save
+          super
+          self.topic_id||=self.class.max(:topic_id).to_i+1
         end
         def reply
           foo=reggae_instance.set_values(:parent_id => id,:title => "re: #{title}")
           foo.new_rec=true
+          foo.url=self.class.new.url
           foo.schema.delete_if {|c| [:author,:id].include?(c[NAME_INDEX])}
           foo
         end
