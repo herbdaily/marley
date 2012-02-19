d9cadc86ebe0a7cba4713720953d25c369355721
Herb Daily <herb.daily@safe-mail.net>
2012-02-18 21:59:24 -0300

    more work on forum joint

--- lib/marley/joints/forum.rb
+++ lib/marley/joints/forum.rb
@@ -1,6 +1,40 @@
 
 module Marley
   module Plugins
+    class MessageThreading < Plugin
+      def apply(*klasses)
+        super
+        klasses.each {|klass| klass.plugin :tree}
+      end
+      module ClassMethods
+        def topics
+          self.dataset.filter(:parent_id => nil)
+        end
+        def list(params=nil)
+          topics.map{|t| t.thread}
+        end
+      end
+      module InstanceMethods
+        def topic_msg
+          self.class.topics.filter(:topic_id => topic_id).first
+        end
+        def topic
+          self.topic_msg.tree
+        end
+        def children
+          self.class.filter(:parent_id => id).all
+        end
+        def thread
+          children.length > 0 ? reggae_instance << children.map{|m| m.thread} : reggae_instance
+        end
+        def reply
+          foo=reggae_instance.set_values(:parent_id => id,:title => "re: #{title}")
+          foo.new_rec=true
+          foo.schema.delete_if {|c| [:author,:id].include?(c[NAME_INDEX])}
+          foo
+        end
+      end
+    end
   end
   module Joints
     class Forum < Joint
@@ -11,7 +45,7 @@ module Marley
         Marley.joint('section')
         Marley.plugin(:section).apply('PrivateMessage')
         Marley.plugin(:section).apply('PublicMessage')
-
+        Marley.plugin(:message_threading).apply('PublicMessage')
       end
     end
   end
--- rdoc/forum_joint.rb
+++ rdoc/forum_joint.rb
@@ -13,6 +13,8 @@ DB.create_table :users do
 end
 DB.create_table :messages do
   primary_key :id
+  integer :topic_id, :index => true
+  integer :parent_id, :index => true
   integer :user_id, :index => true
   text :message_type, :index => true
   text :title,  :index => true,:null => false
@@ -27,13 +29,20 @@ DB.create_table :messages_users do
 end
 DB.create_table :tags do
   primary_key :id
-  integer :user_id
-  text :tag
+  integer :user_id,:index => true
+  text :tag,:index => true
 end
 DB.create_table :messages_tags do
   primary_key :id
   integer :tag_id
   integer :message_id
+  index [:tag_id, :message_id], :unique => true
+end
+DB.create_table :topics_tags do
+  primary_key :id
+  integer :tag_id
+  integer :topic_id
+  index [:tag_id, :topic_id], :unique => true
 end
  
 
