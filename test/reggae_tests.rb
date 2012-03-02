c3fbcf5308dff9a03c47f7c8d4fda0fdef72d29e
Herb Daily <herb.daily@safe-mail.net>
2012-03-02 02:10:12 -0300

    trying detest as tdoc alternative, so far so good...

--- test/reggae_tests.rb
+++ test/reggae_tests.rb
@@ -6,11 +6,34 @@ require 'marley/utils.rb'
 require 'marley/reggae.rb'
 
 D 'Generate' do
-  T {@instance=Marley::ReggaeInstance.new({:name => 'foo',:schema => []})==[:instance, {:schema=>[], :name=>"foo"}, []]}
+  T {(@instance=Marley::ReggaeInstance.new({:name => 'foo',:schema => []}))==[:instance, {:schema=>[], :name=>"foo"}, []]}
 
   T {(@link=Marley::ReggaeLink.new({:url => '/', :title => 'asdf'}))==[:link, {:url=>"/", :title=>"asdf"}, []]}
   T {@link.update(:url => '/asdf/')==[:link, {:url=>"/asdf/", :title=>"asdf"}, []]}
 
 end
 D 'parse' do
+  D 'leave non-Reggae arrays as is' do
+    T {Marley::Reggae.new([])==[]}
+    T {Marley::Reggae.new(['asdf','ssss'])==["asdf", "ssss"]}
+    T {Marley::Reggae.new(['asdf','ssss']).class==Marley::Reggae}
+    T {Marley::Reggae.new(['asdf',{}]).to_resource==["asdf", {}]}
+    T {Marley::Reggae.new(['asdf',{}]).to_resource.class==Marley::Reggae}
+  end
+ D 'a simple Reggae Resource' do
+    T {Marley::Reggae.new(['resource',{}]).to_resource.class==Marley::ReggaeResource}
+    T {Marley::Reggae.get_resource(['resource',{}]).class==Marley::ReggaeResource}
+ end
+
+  D 'parse a ReggaeSection' do
+    T {(@section=Marley::Reggae.new([:section, {}]).to_resource)==[:section, {}]}
+    T {@section.properties=={}}
+  end
 end
+
+
+
+
+
+
+#conversion:  cf T {x$a==Jdf $a}j0f>
