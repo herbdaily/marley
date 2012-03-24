f5b8e4cd14bad27309d80fae870e82436e2716ee
Herb Daily <herb.daily@safe-mail.net>
2012-03-24 02:25:16 -0300

    more code cleanup

--- lib/marley/joints/tags.rb
+++ lib/marley/joints/tags.rb
@@ -1,30 +1,15 @@
 module Marley
   module Plugins
     class Tagging < Plugin
-      @default_opts={:join_type => 'many_to_many', :add_to_write_cols? => true}
+      @default_opts={:concat_tags => true}
       def apply(*klasses)
         klasses.each do |klass|
           klass=MR.const_get(klass) if klass.is_a?(String)
-          # not crazy about this nested if shit.  there may be a better way...
-          if val=klass.instance_variable_get('@derived_after_cols')
-            if val[:new?]
-              val[:new?][:all] << @tag_col_name.to_sym
-            else
-              val[:new?]={:all => [@tag_col_name.to_sym]}
-            end
-          else
-            klass.instance_variable_set('@derived_after_cols',{:new? => {:all => [@tag_col_name.to_sym]}})
-          end
+          klass.derived_after_cols![:new?][:all] << @tag_col_name.to_sym
           @instance_methods_mod.send(:append_features,klass)
           tag_class=@tag_class
           join_type=@opts[:"#{klass}_join_type"] || @opts[:join_type]
-          if join_type=='many_to_many'
-            Marley::Utils.many_to_many_join(klass, tag_class)
-          else
-            reciprocal_join=join_type.split('_').reverse.join('_')
-            tag_class.send(reciprocal_join.to_sym, klass.resource_name.pluralize.to_sym, {:class => klass})
-            klass.send(join_type.to_sym, tag_class.resource_name.pluralize.to_sym, {:class => tag_class})
-          end
+          Marley::Utils.many_to_many_join(klass, tag_class)
         end
       end
       def initialize(opts={})
@@ -33,15 +18,14 @@ module Marley
         tag_col_name=@tag_col_name="_#{@tag_type}_tags"
         tag_class=@tag_class=MR.const_get(@tag_col_name.sub(/^_/,'').singularize.camelcase)
         tags_ds_name=@tags_ds_name="#{tag_col_name.sub(/^_/,'')}_dataset"
-        add_to_write_cols=@opts[:add_to_write_cols?]
         @instance_methods_mod=Module.new do |m|
           attr_accessor tag_col_name
-          if add_to_write_cols
-            define_method(:write_cols) {
-              super << tag_col_name.to_sym
-            }
-          end
+          define_method(:write_cols) {
+            super << tag_col_name.to_sym
+          }
 
+          #list dataset should take care of most of this
+          
           define_method("#{tag_col_name}_ds".to_sym) { #e.g. _private_tags_ds
             send(tags_ds_name).filter({:tags__user_id => (tag_class.associations.include?(:user) ? self.class.current_user[:id] : nil)})
           }
