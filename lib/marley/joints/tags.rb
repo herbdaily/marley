95515194c0f2bfc9e08cd622dcc0573c4e309881
Herb Daily <herb.daily@safe-mail.net>
2012-03-27 00:38:51 -0300

    more code cleanup

--- lib/marley/joints/tags.rb
+++ lib/marley/joints/tags.rb
@@ -18,9 +18,10 @@ module Marley
         tag_col_name=@tag_col_name="_#{@tag_type}_tags"
         tag_class=@tag_class=MR.const_get(@tag_col_name.sub(/^_/,'').singularize.camelcase)
         tags_ds_name=@tags_ds_name="#{tag_col_name.sub(/^_/,'')}_dataset"
+        tags_meth=tag_class.resource_name.pluralize.to_sym
         @class_methods_mod=Module.new do |m|
           define_method(:list_dataset) {|*args|
-            super.eager_graph(tag_class.resource_name.pluralize.to_sym => proc{|ds| ds.filter(:tags__user_id => tag_class.associations.include?(:user) ? current_user[:id] : nil)})
+            super.eager_graph(tags_meth => proc{|ds| ds.filter(:tags__user_id => tag_class.associations.include?(:user) ? current_user[:id] : nil)})
           }
         end
         @instance_methods_mod=Module.new do |m|
@@ -28,28 +29,18 @@ module Marley
           define_method(:write_cols) {
             super << tag_col_name.to_sym
           }
-
-          
-          define_method("#{tag_col_name}_ds".to_sym) { #e.g. _private_tags_ds
-            send(tags_ds_name).filter({:tags__user_id => (tag_class.associations.include?(:user) ? self.class.current_user[:id] : nil)})
-          }
           define_method(tag_col_name.to_sym) {    #e.g. _private_tags
-            #send("#{tag_col_name}_ds").map {|t| t.tag}.join(', ') unless new?
-             send(tag_class.resource_name.pluralize.to_sym).map {|t| t.tag}.join(', ') unless new?
-          }
-          define_method("add#{tag_col_name}".to_sym) {|tags|  #e.g. add_private_tags
-            vals_hash={:user_id => (tag_class.associations.include?(:user) ? self.class.current_user[:id] : nil)}
-            tags.to_s.split(',').each {|tag| self.send("add#{tag_col_name.singularize}",tag_class.find_or_create(vals_hash.update(:tag => tag))) }
+            send(tags_meth).map {|t| t.tag}.join(', ') unless new?
           }
           define_method("replace#{tag_col_name}".to_sym) {  #e.g. replace_private_tags
-            send("#{tag_col_name}_ds").each {|tag| send("remove#{tag_col_name}".singularize,tag)}
+            send(tags_meth).each {|tag| send("remove#{tag_col_name}".singularize,tag)}
             send("add#{tag_col_name}",instance_variable_get("@#{tag_col_name}"))
           }
           define_method(:after_save) {
             super
-            methods.select {|m| m.match(/^replace_.+_tags/)}.each do |replace_method|
-              send(replace_method)
-            end
+            send("#{tags_meth}_dataset").filter({:tags__user_id => (tag_class.associations.include?(:user) ? self.class.current_user[:id] : nil)}).each {|tag| send("remove#{tag_col_name}".singularize,tag)}
+            vals_hash={:user_id => (tag_class.associations.include?(:user) ? self.class.current_user[:id] : nil)}
+            instance_variable_get("@#{tag_col_name}").split(/\s*,\s*/).each {|tag| self.send("add#{tag_col_name.singularize}",tag_class.find_or_create(vals_hash.update(:tag => tag))) }
           }
         end
       end
