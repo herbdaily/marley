3aa25b18a9f3c132cea12951c913d61605f53b4f
Herb Daily <herb.daily@safe-mail.net>
2012-03-28 21:41:15 -0300

    broken, but closer.  feeling like a moron :(

--- lib/marley/plugins/orm_manifest_path.rb
+++ lib/marley/plugins/orm_manifest_path.rb
@@ -18,14 +18,33 @@ module Marley
         end
       end
       module InstanceMethods
-        def tree_ds
-          self.class.list_dataset.filter(PATH_COL.like("#{send(PATH_COL)}-#{id}_%")).order(PATH_COL)
-        end
+        def tree_ds; self.class.list_dataset.filter(PATH_COL.like("#{children_path}%")); end
+        def _path;send(PATH_COL);end
+        def path_arr; _path.to_s.split(SEP).map &:to_i; end
+        def root_id; path_arr[0]; end
+        def parent_id; path_arr[-1]; end
+        def children_path;("#{_path}#{id}#{SEP}");end
+        def children_path_arr; children_path.split(SEP).map &:to_i; end
+        def new_child; self.class.new({PATH_COL => children_path}); end
+        def depth; path_arr.length; end
+
         def tree
           res=block_given? ? (yield self) : [self,[]]
-          targets=[res[-1].is_a?(Array) ? res[-1] : res]
-          current_depth=0
-          tree_ds.all.each do |node|
+          tree_ds.all.sort {|x,y| x.children_path_arr <=> y.children_path_arr}.inject(res) do |arr,node|
+            if arr[0].depth<node.depth
+              arr[-1] << node
+            else
+            end
+            current_depth=node.depth
+            arr
+          end
+        end
+        def tree_foo
+          res=block_given? ? (yield self) : [self,[]]
+          targets=[]
+          current_depth=depth
+          targets[current_depth]=res[-1].is_a?(Array) ? res[-1] : res
+          tree_ds.all.sort {|x,y| x.children_path_arr <=> y.children_path_arr}.each do |node|
             n=block_given? ? (yield node) : [node,[]]
             if node.depth>current_depth
               targets[node.depth]=n
@@ -40,18 +59,12 @@ module Marley
           end
           res
         end
-        def depth
-          send(PATH_COL).split(SEP).length
-        end
         def values_tree
           tree do |n|
             n.rest_cols.map{|c| n.send(c)}
           end
         end
         def reggae_tree(node=nil)
-        end
-        def new_child
-          self.class.new({PATH_COL => "#{send(PATH_COL)}#{SEP}#{id}"})
         end
       end
     end
